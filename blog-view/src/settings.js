aff680bafa18eb04a766c4d5b420115fc853511c
chaobk <1004945427@qq.com>
2024-03-29 21:06:25 +0800

    更新首页图片展示

--- blog-view/src/settings.js
+++ blog-view/src/settings.js
@@ -1,11 +1,13 @@
 module.exports = {
+
 	/**
 	 * @type {string}
 	 * @description 首页三张背景图
 	 */
-	bg1: 'https://cdn.naccl.top/blog/img/bg1.jpg',
-	bg2: 'https://cdn.naccl.top/blog/img/bg2.jpg',
-	bg3: 'https://cdn.naccl.top/blog/img/bg3.jpg',
+	bg1: 'https://chaobk-repo.oss-cn-shenzhen.aliyuncs.com/pic/136.jpg',
+	bg2: 'https://chaobk-repo.oss-cn-shenzhen.aliyuncs.com/pic/13.jpg',
+	bg3: 'https://chaobk-repo.oss-cn-shenzhen.aliyuncs.com/pic/119.jpg',
+
 
 	/**
 	 * @type {string}
