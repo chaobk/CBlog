7c6ec48d1a5c34b91e6eddc26866d737ebba3eac
chaobk <1004945427@qq.com>
2024-04-11 21:57:41 +0800

    引入knife4j依赖，之后逐步补充接口文档

--- blog-api/src/main/java/com/chaobk/controller/AboutController.java
+++ blog-api/src/main/java/com/chaobk/controller/AboutController.java
@@ -3,6 +3,8 @@ package com.chaobk.controller;
 import com.chaobk.annotation.VisitLogger;
 import com.chaobk.enums.VisitBehavior;
 import com.chaobk.model.vo.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +16,7 @@ import com.chaobk.service.AboutService;
  * @Date: 2020-08-31
  */
 @RestController
+@Api("关于我页面")
 public class AboutController {
 	@Autowired
 	AboutService aboutService;
@@ -23,6 +26,7 @@ public class AboutController {
 	 *
 	 * @return
 	 */
+	@ApiOperation("获取关于我页面信息")
 	@VisitLogger(VisitBehavior.ABOUT)
 	@GetMapping("/about")
 	public Result about() {
