a8728d5d103ff6d947ffd3124532079bcc9dab3a
chaobk <1004945427@qq.com>
2024-04-18 22:02:51 +0800

    Revert "Revert "升级springboot版本，处理pagehelper循环依赖问题""
    This reverts commit ae02cc42e422164becdacb70aa4aab0ca0b40426.

--- blog-api/src/main/java/com/chaobk/controller/AboutController.java
+++ blog-api/src/main/java/com/chaobk/controller/AboutController.java
@@ -3,12 +3,12 @@ package com.chaobk.controller;
 import com.chaobk.annotation.VisitLogger;
 import com.chaobk.enums.VisitBehavior;
 import com.chaobk.model.vo.Result;
+import com.chaobk.service.AboutService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.chaobk.service.AboutService;
 
 /**
  * @Description: 关于我页面
@@ -16,10 +16,10 @@ import com.chaobk.service.AboutService;
  * @Date: 2020-08-31
  */
 @RestController
-@Api("关于我页面")
+@Api(tags = "AboutController - 关于我页面")
+@RequiredArgsConstructor
 public class AboutController {
-	@Autowired
-	AboutService aboutService;
+	private final AboutService aboutService;
 
 	/**
 	 * 获取关于我页面信息
