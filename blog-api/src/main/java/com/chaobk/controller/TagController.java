88a5d9c8c02a77495ad9ef85312dcdaf5ff3001e
chaobk <1004945427@qq.com>
2024-04-23 21:43:05 +0800

    补充部分接口文档，删除部分冗余代码

--- blog-api/src/main/java/com/chaobk/controller/TagController.java
+++ blog-api/src/main/java/com/chaobk/controller/TagController.java
@@ -1,15 +1,19 @@
 package com.chaobk.controller;
 
+import com.chaobk.annotation.VisitLogger;
 import com.chaobk.enums.VisitBehavior;
 import com.chaobk.model.vo.BlogInfo;
 import com.chaobk.model.vo.PageResult;
 import com.chaobk.model.vo.Result;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.chaobk.service.BlogService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.chaobk.annotation.VisitLogger;
-import com.chaobk.service.BlogService;
 
 /**
  * @Description: 标签
@@ -17,9 +21,11 @@ import com.chaobk.service.BlogService;
  * @Date: 2020-08-17
  */
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/tag")
+@Api(tags = "TagController - 标签")
 public class TagController {
-	@Autowired
-	BlogService blogService;
+	private final BlogService blogService;
 
 	/**
 	 * 根据标签name分页查询公开博客列表
@@ -29,9 +35,10 @@ public class TagController {
 	 * @return
 	 */
 	@VisitLogger(VisitBehavior.TAG)
-	@GetMapping("/tag")
-	public Result tag(@RequestParam String tagName,
-                      @RequestParam(defaultValue = "1") Integer pageNum) {
+	@GetMapping
+	@ApiOperation("根据标签name分页查询公开博客列表")
+	public Result tag(@ApiParam("标签名") @RequestParam String tagName,
+                      @ApiParam("页码") @RequestParam(defaultValue = "1") Integer pageNum) {
 		PageResult<BlogInfo> pageResult = blogService.getBlogInfoListByTagNameAndIsPublished(tagName, pageNum);
 		return Result.ok("请求成功", pageResult);
 	}
